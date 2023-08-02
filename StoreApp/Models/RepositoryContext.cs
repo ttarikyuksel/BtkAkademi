910f04b46f706e9b0d2ac0f596b308f6f2658be8
Tarik Yuksel <yukseltarik@hotmail.com>
2023-08-02 20:26:12 +0300

    2082023

--- StoreApp/Models/RepositoryContext.cs
+++ StoreApp/Models/RepositoryContext.cs
@@ -4,10 +4,25 @@ namespace StoreApp.Models
 {
     public class RepositoryContext : DbContext
     {
-        public RepositoryContext(DbContextOptions<RepositoryContext> options) :base(options)
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
         {
-            
+
         }
         public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>()
+            .HasData(
+                new Product() { Id = 1, ProductName = "Computer", Price = 17_000 },
+                new Product() { Id = 2, ProductName = "Keyboard", Price = 1_000 },
+                new Product() { Id = 3, ProductName = "Mouse", Price = 500 },
+                new Product() { Id = 4, ProductName = "Monitor", Price = 7_000 },
+                new Product() { Id = 5, ProductName = "Deck", Price = 1_500 }
+            );
+        }
+
     }
 }
