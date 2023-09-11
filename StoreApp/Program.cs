6047c140d38d7facdc62437ac684228ba4f319de
Tarik Yuksel <yukseltarik@hotmail.com>
2023-09-11 20:58:18 +0300

    091120232

--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -29,7 +29,18 @@ app.UseStaticFiles();
 app.UseHttpsRedirection();
 app.UseRouting();
 
-app.MapControllerRoute("default",
-   pattern:"{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapAreaControllerRoute(
+        name:"Admin",
+        areaName : "Admin" ,
+        pattern : "Admin/{controller=Dashboard}/{action=Index}/{id?}"
+        );
+
+    endpoints.MapControllerRoute(
+        name:"default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+        );
+});
 
 app.Run();
