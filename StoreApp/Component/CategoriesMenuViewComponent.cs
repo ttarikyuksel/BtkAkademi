67423bc1a1b6f18267a003845da4579a29fcb165
Mustafa Tarık Yüksel <89916905+ttarikyuksel@users.noreply.github.com>
2023-09-11 15:02:12 +0300

    20230911

---
+++ StoreApp/Component/CategoriesMenuViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace StoreApp.Component
+{
+    public class CategoriesMenuViewComponent : ViewComponent
+    {
+        private readonly IServiceManager _manager;
+
+        public CategoriesMenuViewComponent(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _manager.CategoryService.GetAllCategoeris(false);
+            return View(categories);
+        }
+    }
+}
